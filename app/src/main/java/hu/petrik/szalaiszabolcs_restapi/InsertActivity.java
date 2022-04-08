b4c936919c8db3ffc587b29a3aa9bb07a4aa816e
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-04-08 03:55:56 +0200

    layoutok

--- app/src/main/java/hu/petrik/szalaiszabolcs_restapi/InsertActivity.java
+++ app/src/main/java/hu/petrik/szalaiszabolcs_restapi/InsertActivity.java
@@ -2,13 +2,26 @@ package hu.petrik.szalaiszabolcs_restapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import hu.petrik.szalaiszabolcs_restapi.databinding.ActivityInsertBinding;
+import hu.petrik.szalaiszabolcs_restapi.databinding.ActivityListResultBinding;
+
 public class InsertActivity extends AppCompatActivity {
+
+    ActivityInsertBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_insert);
+        binding = ActivityInsertBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnVissza.setOnClickListener(view -> {
+            Intent intent = new Intent(InsertActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
--- app/src/main/java/hu/petrik/szalaiszabolcs_restapi/ListResultActivity.java
+++ app/src/main/java/hu/petrik/szalaiszabolcs_restapi/ListResultActivity.java
@@ -2,13 +2,26 @@ package hu.petrik.szalaiszabolcs_restapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import hu.petrik.szalaiszabolcs_restapi.databinding.ActivityListResultBinding;
+import hu.petrik.szalaiszabolcs_restapi.databinding.ActivityMainBinding;
+
 public class ListResultActivity extends AppCompatActivity {
+
+    ActivityListResultBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_result);
+        binding = ActivityListResultBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnVissza.setOnClickListener(view -> {
+            Intent intent = new Intent(ListResultActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
