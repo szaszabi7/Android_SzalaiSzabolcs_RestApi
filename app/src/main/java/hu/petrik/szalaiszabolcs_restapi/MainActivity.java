86248162070a7a5b19d784f0543d32dbdc0edfac
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-04-08 03:44:00 +0200

    viewbinding, layoutok

--- app/src/main/java/hu/petrik/szalaiszabolcs_restapi/MainActivity.java
+++ app/src/main/java/hu/petrik/szalaiszabolcs_restapi/MainActivity.java
@@ -2,13 +2,36 @@ package hu.petrik.szalaiszabolcs_restapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.LauncherApps;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import hu.petrik.szalaiszabolcs_restapi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnListazas.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ListResultActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        binding.btnUj.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    private void init() {
     }
 }
